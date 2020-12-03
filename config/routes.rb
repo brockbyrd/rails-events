42315cb09f5badb6d47b064647ff11a3acb21f83
brockbyrd <brockbyrdd@gmail.com>
2020-12-03 15:20:31 -0600

    "creates scope routes"

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,23 @@ Rails.application.routes.draw do
     root to: 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :arenas, only: [:index, :show, :new, :edit]
-  resources :events, only: [:index, :show, :new, :edit]
+  
+  get '/events/comedy', to: 'events#comedy'
+  get '/events/sports', to: 'events#sports'
+  get '/events/concert', to: 'events#concert'
+  
+  resources :arenas do
+    resources :events, only: [:index, :show, :new, :edit]
+  end
+  resources :events
+
+  devise_scope :user do
+    get '/users/sign_in', to: 'devise/sessions#new'
+    post '/users/sign_in', to: 'devise/sessions#create'
+    get '/users/sign_up', to: 'devise/sessions#new'
+    post '/users/sign_up', to: 'devise/sessions#create'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 
   get '/users/:id', to: 'users#show'
 end
