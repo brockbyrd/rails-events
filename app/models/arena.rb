221c788c3d3dc7d6b0795afa26023f78666ec37d
brockbyrd <brockbyrdd@gmail.com>
2020-12-03 14:47:07 -0600

    "validations for arenas"

--- app/models/arena.rb
+++ app/models/arena.rb
@@ -1,4 +1,6 @@
 class Arena < ApplicationRecord
     has_many :events
     has_many :users, through: :events
+    validates :name, :city, :state, presence: true
+    validates :name, uniqueness: true
 end
